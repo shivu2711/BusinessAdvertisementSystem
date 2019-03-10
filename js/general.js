ccbbc9bd5268edb6ee7506b7ea858e75adc7cad5
shivu271198 <shivaniponda@gmail.com>
2019-03-10 11:47:04 +0530

    Business Advertisement System

---
+++ js/general.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    
+
+    /* Select Device Category Carousel */
+    $('.main-banner-slider').owlCarousel({
+    	loop: true,
+        items:1,
+    	nav: true,
+    	navigation: true,
+    	dots: false,
+    	
+    });
+
+
+    });
+
+
+
---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.write(
+	 '<script type="text/javascript" src="js/jquery.js"></script>' +
+    '<script type="text/javascript" src="js/bootstrap.min.js"></script>'+
+    '<script type="text/javascript" src="js/owl.carousel.min.js"></script>'+
+    '<script type="text/javascript" src="js/general.js"></script>'
+    );
