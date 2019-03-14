474548d25f85326d34955d795f2ca086fe51ad06
shivu271198 <shivaniponda@gmail.com>
2019-03-14 17:26:26 +0530

    index cmplt

--- js/general.js
+++ js/general.js
@@ -1,28 +1,50 @@
 
-function footerAdj(){
-    var footerH=$('footer').outerHeight();
-    $("footer").css({"margin-top": -footerH});
-    $(".wrapper").css({"padding-bottom":footerH});
+function footerAdj() {
+    var footerH = $('footer').outerHeight();
+    $("footer").css({ "margin-top": -footerH });
+    $(".wrapper").css({ "padding-bottom": footerH });
 }
 
 
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
 
     /* Select Device Category Carousel */
     $('.main-banner-slider').owlCarousel({
-    	loop: true,
-        items:1,
-    	nav: true,
-    	navigation: true,
-    	dots: false,
-    	
-    });
-   
+        loop: true,
+        items: 1,
+        nav: true,
+        navigation: true,
+        dots: false,
 
     });
-$(window).resize(function(){
-footerAdj();
+    $('.inner-banner-slider').owlCarousel({
+        loop: true,
+        items: 1,
+        nav: true,
+        navigation: true,
+        dots: false,
+
+    });
+    $("#normal_select").dropkick();
+    $('.bottom-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        navigation: true,
+        margin:10,
+        items: 5,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause:false
+    });
+
+
+
+
+});
+$(window).resize(function () {
+    footerAdj();
 });
 
 
